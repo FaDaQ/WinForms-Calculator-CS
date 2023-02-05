42dfd703b1a9da2717dcdead116920e0733ec3b3
Михаил Дёмин <dymin2018@gmail.com>
2023-02-05 13:45:34 +0400

    Полностью переработан дизайн, добавлена кнопка "Стереть всё"

--- Калькулятор GUI V2/Form1.Designer.cs
+++ Калькулятор GUI V2/Form1.Designer.cs
@@ -28,51 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.ExpressionPanel = new System.Windows.Forms.Panel();
             this.Expression = new System.Windows.Forms.Label();
-            this.panel1.SuspendLayout();
+            this.MinimizeButton = new System.Windows.Forms.Button();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.ExpressionPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
+            // ExpressionPanel
             // 
-            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.ExpressionPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.panel1.Controls.Add(this.Expression);
-            this.panel1.Location = new System.Drawing.Point(12, 21);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(442, 100);
-            this.panel1.TabIndex = 0;
+            this.ExpressionPanel.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ExpressionPanel.Controls.Add(this.Expression);
+            this.ExpressionPanel.Location = new System.Drawing.Point(4, 12);
+            this.ExpressionPanel.Name = "ExpressionPanel";
+            this.ExpressionPanel.Size = new System.Drawing.Size(560, 100);
+            this.ExpressionPanel.TabIndex = 0;
             // 
             // Expression
             // 
+            this.Expression.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.Expression.AutoSize = true;
-            this.Expression.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Expression.BackColor = System.Drawing.Color.Transparent;
+            this.Expression.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Expression.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Expression.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.Expression.Location = new System.Drawing.Point(3, 29);
             this.Expression.Name = "Expression";
-            this.Expression.Size = new System.Drawing.Size(37, 45);
+            this.Expression.Size = new System.Drawing.Size(40, 45);
             this.Expression.TabIndex = 0;
             this.Expression.Text = "0";
             this.Expression.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // MinimizeButton
+            // 
+            this.MinimizeButton.BackColor = System.Drawing.Color.DimGray;
+            this.MinimizeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.MinimizeButton.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.MinimizeButton.Location = new System.Drawing.Point(503, -2);
+            this.MinimizeButton.Name = "MinimizeButton";
+            this.MinimizeButton.Size = new System.Drawing.Size(40, 23);
+            this.MinimizeButton.TabIndex = 2;
+            this.MinimizeButton.Text = " ‾";
+            this.MinimizeButton.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.MinimizeButton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.MinimizeButton.UseVisualStyleBackColor = false;
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.BackColor = System.Drawing.Color.Brown;
+            this.CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CloseButton.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CloseButton.Location = new System.Drawing.Point(544, -2);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(40, 23);
+            this.CloseButton.TabIndex = 1;
+            this.CloseButton.Text = " X";
+            this.CloseButton.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.CloseButton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.CloseButton.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(466, 639);
-            this.Controls.Add(this.panel1);
-            this.MinimumSize = new System.Drawing.Size(482, 678);
+            this.ClientSize = new System.Drawing.Size(584, 661);
+            this.Controls.Add(this.MinimizeButton);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.ExpressionPanel);
+            this.MinimumSize = new System.Drawing.Size(600, 700);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.ExpressionPanel.ResumeLayout(false);
+            this.ExpressionPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private Panel panel1;
+        private Panel ExpressionPanel;
         private Label Expression;
+        private Button CloseButton;
+        private Button MinimizeButton;
     }
 }
--- Калькулятор GUI V2/Form1.cs
+++ Калькулятор GUI V2/Form1.cs
@@ -1,4 +1,5 @@
 using System.Data;
+using System.Runtime.InteropServices;
 
 namespace Калькулятор_GUI_V2
 {
@@ -8,7 +9,9 @@ namespace Калькулятор_GUI_V2
         {
             public ACalcButton()
             {
-                mainButton.Font = new Font("Arial", 20);
+                mainButton.Font = new Font("Comic Sans MS", 20);
+                mainButton.FlatStyle = FlatStyle.Flat;
+                mainButton.FlatAppearance.BorderSize = 0;
             }
 
             public void Place(Form form)
@@ -25,13 +28,15 @@ namespace Калькулятор_GUI_V2
             public Button mainButton { get; protected set; } = new Button();
             public Label Label { get; protected set; } = new Label();
         }
-        class DigitButton : ACalcButton
+        class StdCalcButton : ACalcButton
         {
-            public DigitButton(int digit, Label label, Point location, int width, int height)
+            public StdCalcButton(int digit, Label label, Point location, int width, int height)
             {
                 mainButton.Size = new Size(width, height);
                 mainButton.Location = location;
                 mainButton.Text = digit.ToString();
+                mainButton.BackColor = Color.FromArgb(19, 20, 19);
+                mainButton.ForeColor = Color.White;
                 Label = label;
                 mainButton.Click += Click;
             }
@@ -43,6 +48,8 @@ namespace Калькулятор_GUI_V2
                 mainButton.Size = new Size(width, height);
                 mainButton.Location = location;
                 mainButton.Text = "=";
+                mainButton.BackColor = Color.FromArgb(19, 20, 19);
+                mainButton.ForeColor = Color.White;
                 mainButton.Click += Click;
                 Label = label;
             }
@@ -71,6 +78,7 @@ namespace Калькулятор_GUI_V2
                 mainButton.Size = new Size(width, height);
                 mainButton.Location = location;
                 mainButton.Text = actSign;
+                mainButton.BackColor = Color.FromArgb(201, 121, 34);
                 Label = label;
                 mainButton.Click += Click;
             }
@@ -83,10 +91,12 @@ namespace Калькулятор_GUI_V2
                 mainButton.Location = location;
                 mainButton.Text = text;
                 Label = label;
+                mainButton.BackColor = Color.FromArgb(19, 20, 19);
+                mainButton.ForeColor = Color.White;
                 mainButton.Click += Click;
             }
 
-            protected override void Click(object sender, EventArgs e)
+            new private void Click(object sender, EventArgs e)
             {
                 try
                 {
@@ -98,51 +108,115 @@ namespace Калькулятор_GUI_V2
                 }
             }
         } 
+        class ClearButton : ACalcButton
+        {
+            public ClearButton(string clearSymbol, Label label, Point location, int width, int height)
+            {
+                mainButton.Size = new Size(width, height);
+                mainButton.Location = location;
+                mainButton.Text = clearSymbol;
+                mainButton.BackColor = Color.FromArgb(19, 20, 19);
+                mainButton.ForeColor = Color.White;
+                Label = label;
+                mainButton.Click += Click;
+            }
+
+            new private void Click(object sender, EventArgs e)
+            {
+                Label.Text = "0";
+            }
+        }
 
         public Form1()
         {
             InitializeComponent();
             this.Text = "GUI Калькулятор v2";
+            this.BackColor = Color.FromArgb(43, 42, 40);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.ExpressionPanel.BackColor = Color.Transparent;
+            this.CloseButton.Click += Close;
+            this.MinimizeButton.Click += Minimize;
+            this.CloseButton.FlatAppearance.BorderSize = 0;
+            this.MinimizeButton.FlatAppearance.BorderSize = 0;
+            this.MouseDown += DragWindow;
 
             List<ACalcButton> calcBtns = new List<ACalcButton>();
 
-            int FirstBtnPosX = 40;
-            int digitBtnWidth = 100;
-            int digitBtnHeight = 100;
-            Point buttonPos = new Point(FirstBtnPosX, 140);
-            calcBtns.Add(new EraseButton("ᐸ", Expression, new Point(FirstBtnPosX + 200, 540), digitBtnWidth, 80));
+            int startBtnPosX = (this.Size.Width / 2) - 200;
+            int endBtnPosX = 300;
+            int startBtnPosY = 140;
+            int stdBtnWidth = 100;
+            int stdBtnHeight = 100;
+            Point btnPos = new Point(startBtnPosX, startBtnPosY);
 
-            for (int i = 1; i <= 9; i++)
+            foreach (int digit in "123456789".Select(digit => int.Parse(digit.ToString())))
             {
-                calcBtns.Add(new DigitButton(i, Expression, buttonPos, digitBtnWidth, digitBtnHeight));
-                if (buttonPos.X < 212)
-                    buttonPos.X += digitBtnWidth;
-                else
+                calcBtns.Add(new StdCalcButton(digit, Expression, btnPos, stdBtnWidth, stdBtnHeight));
+                btnPos = (btnPos.X >= endBtnPosX) ? new Point(startBtnPosX, btnPos.Y + stdBtnHeight)
+                                                  : new Point(btnPos.X + stdBtnWidth, btnPos.Y);
+                switch (digit)
                 {
-                    buttonPos.X = FirstBtnPosX;
-                    buttonPos.Y += digitBtnHeight;
+                    case 2:
+                        int posY = 0; //Дополнительная позиция по Y, чтобы смещать кнопки вниз
+                        int posX = btnPos.X + 100; //Дополнительная позиция по X, чтобы смещать кнопки влево
+                        foreach (char symb in "+-*/")
+                        {
+                            calcBtns.Add(new ActionSignButton(symb.ToString(), Expression,
+                                         new Point(btnPos.X + 100, btnPos.Y + posY), stdBtnWidth - 10, stdBtnHeight));
+                            posY += 100;
+                        }
+                        calcBtns.Add(new EraseButton("<", Expression,
+                                     new Point(btnPos.X + stdBtnWidth, btnPos.Y + posY), stdBtnWidth - 10, stdBtnHeight));
+
+                        posY -= 100;
+                        calcBtns.Add(new ActionSignButton(".", Expression,
+                                     new Point(posX -= stdBtnWidth, btnPos.Y + posY), stdBtnWidth, stdBtnHeight));
+                        calcBtns.Add(new EqualButton(Expression,
+                                     new Point(posX, btnPos.Y + posY + stdBtnHeight), stdBtnWidth, stdBtnHeight));
+                        calcBtns.Add(new StdCalcButton(0, Expression,
+                                     new Point(posX -= stdBtnWidth, btnPos.Y + posY), stdBtnWidth, stdBtnHeight));
+                        calcBtns.Add(new ClearButton("C", Expression,
+                                     new Point(posX -= stdBtnWidth, btnPos.Y + posY), stdBtnWidth, stdBtnHeight));
+                        calcBtns.Add(new ActionSignButton("(", Expression,
+                                     new Point(posX, btnPos.Y + (posY += stdBtnHeight)),
+                                     stdBtnWidth, stdBtnHeight));
+                        calcBtns.Add(new ActionSignButton(")", Expression,
+                                     new Point(posX += stdBtnWidth, btnPos.Y + posY),
+                                     stdBtnWidth, stdBtnHeight));
+                        break;
                 }
-
+                
             }
-
-            calcBtns.Add(new ActionSignButton("%", Expression, new Point(FirstBtnPosX, buttonPos.Y), digitBtnWidth, digitBtnHeight));
-            calcBtns.Add(new ActionSignButton("*", Expression, new Point(buttonPos.X + 300, buttonPos.Y), 80, 100));
-            calcBtns.Add(new ActionSignButton("/", Expression, new Point(buttonPos.X + 300, buttonPos.Y -= 100), 80, 100));
-            calcBtns.Add(new ActionSignButton("+", Expression, new Point(buttonPos.X + 300, buttonPos.Y -= 100), 80, 100));
-            calcBtns.Add(new ActionSignButton("-", Expression, new Point(buttonPos.X + 300, buttonPos.Y -= 100), 80, 100));
-
-            buttonPos = new Point(FirstBtnPosX, 440);
-            calcBtns.Add(new DigitButton(0, Expression, new Point(buttonPos.X += digitBtnWidth, buttonPos.Y), digitBtnWidth, digitBtnHeight));
-            
-            calcBtns.Add(new EqualButton(Expression, new Point(buttonPos.X += digitBtnWidth, buttonPos.Y), digitBtnWidth, digitBtnHeight));
-
-            buttonPos.X += 180; buttonPos.Y -= 300;
-            calcBtns.Add(new ActionSignButton(")", Expression, buttonPos, 40, digitBtnHeight));
-            buttonPos.X = FirstBtnPosX - 40;
-            calcBtns.Add(new ActionSignButton("(", Expression, buttonPos, 40, digitBtnHeight));
 
             foreach (var button in calcBtns)
                 button.Place(this);
+
         }
+
+        private void Minimize(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        private void Close(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void DragWindow(object sender,
+        System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd,
+                         int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
     }
 }
